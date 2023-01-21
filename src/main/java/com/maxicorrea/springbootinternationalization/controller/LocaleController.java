b7eb757699851ebd65cd0b3d74800b355dfd7178
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-01-21 12:50:24 -0300

    Internationalization Example

---
+++ src/main/java/com/maxicorrea/springbootinternationalization/controller/LocaleController.java
@@ -0,0 +1,30 @@
+package com.maxicorrea.springbootinternationalization.controller;
+
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("locale")
+public class LocaleController {
+
+  private final MessageSource messageSource;
+
+  @Autowired
+  public LocaleController(
+      final MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @GetMapping("/hello")
+  public String sayHello(
+      @RequestHeader(name = "Accept-Language", required=false) Locale locale) {
+    return messageSource.getMessage("common.hello", null, LocaleContextHolder.getLocale());
+  }
+
+}
