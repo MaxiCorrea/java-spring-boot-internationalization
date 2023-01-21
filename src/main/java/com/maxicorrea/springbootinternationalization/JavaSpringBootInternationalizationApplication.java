8e2b882efe94d92530683c40da6bfa85891c1c01
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-01-21 12:12:54 -0300

    Initial Commit

---
+++ src/main/java/com/maxicorrea/springbootinternationalization/JavaSpringBootInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package com.maxicorrea.springbootinternationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootInternationalizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxicorrea/springbootinternationalization/JavaSpringBootInternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxicorrea.springbootinternationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootInternationalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
