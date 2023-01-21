7ebe3f9d94ee890e73fb1fc95067a418f8309f75
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-01-21 13:09:23 -0300

    Format Code

--- src/main/java/com/maxicorrea/springbootinternationalization/JavaSpringBootInternationalizationApplication.java
+++ src/main/java/com/maxicorrea/springbootinternationalization/JavaSpringBootInternationalizationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringBootInternationalizationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringBootInternationalizationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSpringBootInternationalizationApplication.class, args);
+  }
 
 }
